13eaf6ec4cb8cddf9c179286d9178b3abc46d195
Vera Tikhonova <veratihonova95@gmail.com>
2022-02-21 01:25:11 +0500

    added functions for obtaining random numbers

---
+++ js/main.js
@@ -0,0 +1,45 @@
+/**
+ * Получение случайного целого числа из заданного диапазона
+ * @param {number} min минимальное значение диапазона от нуля и выше
+ * @param {number} max максимальное значение диапазона
+ * @returns {number} случайное число
+ */
+function getRandomInteger(min,max) {
+  if (min < 0 || max < 0) {
+    return undefined;
+  }
+  if (min > max) {
+    let minBefore = min;
+    min = max;
+    max = minBefore;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
+ * Получение случайного с плавающей точкой из диапазона
+ * @param {number} min минимальное значение диапазона от нуля и выше
+ * @param {number} max максимальное значение диапазона
+ * @param {number} decimal количество знаков после запятой
+ * @returns {number} случайное число с плавающей точкой
+ */
+function getRandomFloat(min,max,decimal) {
+  if (min < 0 || max < 0) {
+    return undefined;
+  }
+  if (min > max) {
+    let minBefore = min;
+    min = max;
+    max = minBefore;
+  }
+  return Number((Math.random() * (max - min) + min).toFixed(decimal));
+}
+
+let min = 5;
+let max = 10;
+let decimal = 2;
+
+console.log('Случайное целое число: ' + getRandomInteger(min,max));
+console.log('Случайное число с плавающей точкой: ' + getRandomFloat(min,max,decimal));
+
+
