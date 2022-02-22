5f2e05bf53de1580248b059763ce8638ca45f9b9
Vera Tikhonova <veratihonova95@gmail.com>
2022-02-22 21:57:20 +0500

    corrected remarks

--- js/main.js
+++ js/main.js
@@ -1,31 +1,13 @@
 /**
- * Получение случайного целого числа из заданного диапазона
+ * Получение случайного числа из заданного диапазона
  * @param {number} min минимальное значение диапазона от нуля и выше
  * @param {number} max максимальное значение диапазона
- * @returns {number} случайное число
+ * @param {number} [decimal=0] количество знаков после запятой
+ * @returns {number} случайное число из заданного диапазона
  */
-function getRandomInteger(min,max) {
+function getRandomNumber(min, max, decimal) {
   if (min < 0 || max < 0) {
-    return undefined;
-  }
-  if (min > max) {
-    const minBefore = min;
-    min = max;
-    max = minBefore;
-  }
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-/**
- * Получение случайного с плавающей точкой из диапазона
- * @param {number} min минимальное значение диапазона от нуля и выше
- * @param {number} max максимальное значение диапазона
- * @param {number} decimal количество знаков после запятой
- * @returns {number} случайное число с плавающей точкой
- */
-function getRandomFloat(min,max,decimal) {
-  if (min < 0 || max < 0) {
-    return undefined;
+    throw new Error('Диапазон должен состоять из положительных цифр, включая ноль');
   }
   if (min > max) {
     const minBefore = min;
@@ -35,5 +17,4 @@ function getRandomFloat(min,max,decimal) {
   return Number((Math.random() * (max - min) + min).toFixed(decimal));
 }
 
-getRandomInteger(5,10);
-getRandomFloat(2.3,6.8,2);
+getRandomNumber(2.3,6.8,2);
