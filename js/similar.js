35b67a247b43be90ffae24d1cd3430f286f66cc9
Vera Tikhonova <veratihonova95@gmail.com>
2022-03-19 16:02:16 +0500

    generating markup for similar ads

--- js/similar.js
+++ js/similar.js
@@ -1,17 +1,12 @@
 import {generateAdsNearby} from "./data.js";
 
-const adsList = document.querySelector('.map__canvas');
-const similarCardTemplate = document.querySelector('#card')
-  .content
-  .querySelector('.popup');
-
-const cardElement = similarCardTemplate.cloneNode();
-adsList.appendChild(cardElement);
-
-const similarAds = generateAdsNearby();
-
-const similarListFragment = document.createDocumentFragment();
-
+const renamingTypes = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель'
+}
 const generateFeatureList = (card, features) => {
   const featuresContainer = card.querySelector('.popup__features');
   const featureList = featuresContainer.querySelectorAll('.popup__feature');
@@ -25,20 +20,41 @@ const generateFeatureList = (card, features) => {
     }
   });
 }
+const renderPhotos = (templateElement, data) => {
+  const photosFragment = document.createDocumentFragment();
+  const photosContainer = templateElement.querySelector('.popup__photos');
+  const photoItem = photosContainer.querySelector('.popup__photo');
+
+  data.forEach((photo) => {
+    const photoTemplate = photoItem.cloneNode();
+    photoTemplate.src = photo;
+    photosFragment.append(photoTemplate);
+  })
+  photosContainer.innerHTML = '';
+  photosContainer.append(photosFragment);
+}
+
+const adsList = document.querySelector('.map__canvas');
+const similarCardTemplate = document.querySelector('#card')
+  .content
+  .querySelector('.popup');
+const cardElement = similarCardTemplate.cloneNode();
+adsList.appendChild(cardElement);
+const similarAds = generateAdsNearby();
+const similarListFragment = document.createDocumentFragment();
 
 similarAds.forEach(({author, offer}) => {
-
   const cardElement = similarCardTemplate.cloneNode(true);
   cardElement.querySelector('.popup__avatar').src = author.avatar;
   cardElement.querySelector('.popup__title').textContent = offer.title;
   cardElement.querySelector('.popup__text--address').textContent = offer.address;
   cardElement.querySelector('.popup__text--price').insertHTML = '<p class="popup__text popup__text--price">offer.price<span> ₽/ночь</span></p>';
-  cardElement.querySelector('.popup__type').textContent = offer.type; //нужно сопоставить
+  cardElement.querySelector('.popup__type').textContent = renamingTypes[offer.type];
   cardElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
   cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
-  generateFeatureList (cardElement, offer.features);
-  cardElement.querySelector('.popup__description').textContent = offer.description;
-  cardElement.querySelector('.popup__photo').src=offer.photos[0];
+  offer.features!==undefined ? generateFeatureList (cardElement, offer.features) : cardElement.querySelector('.popup__features').remove();
+  offer.description!==undefined ? cardElement.querySelector('.popup__description').textContent = offer.description : cardElement.querySelector('.popup__description').remove();
+  offer.photos!==undefined ? renderPhotos (cardElement, offer.photos) : cardElement.querySelector('.popup__photos').remove();
 
   similarListFragment.appendChild(cardElement);
 });
