eac5c428a980b11c5fac053572a5390b29f8bbb0
Keks <keks@htmlacademy.ru>
2022-04-06 09:57:20 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/user-modal.js
@@ -0,0 +1,7 @@
+import {disableFormFilters} from './form-filters.js';
+import {disableFormAddAds} from './form-add-ads.js';
+import {renderMap} from './map.js';
+
+disableFormAddAds();
+disableFormFilters();
+renderMap();
